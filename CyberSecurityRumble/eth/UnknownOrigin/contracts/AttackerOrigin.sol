05249af3e3758e6f1fd18f61d099f485ced77135
Eddie <31140082+edcod3@users.noreply.github.com>
2021-11-30 23:22:44 +0100

    fix oopsie

--- CyberSecurityRumble/eth/UnknownOrigin/contracts/AttackerOrigin.sol
+++ CyberSecurityRumble/eth/UnknownOrigin/contracts/AttackerOrigin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.0;
 import "./UnknownOrigin.sol";
 
 contract AttackerOrigin {
-    UnknownOrigin public unknownOrig = UnknownOrigin(0xF387B9F6C4EB0735d18c9def34Ba6b21593E74aE);
+    UnknownOrigin public unknownOrig = UnknownOrigin(YOUR_CHALLENGE_CONTRACT_ADDRESS);
 
     function attackOwner(address _owner) public {
         unknownOrig.updateOwner(_owner);
